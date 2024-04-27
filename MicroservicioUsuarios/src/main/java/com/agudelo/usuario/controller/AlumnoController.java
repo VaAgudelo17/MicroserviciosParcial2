0c5e31505ecd0c6093ababd67fd73feca4b412e4
VaAgudelo17 <114451968+VaAgudelo17@users.noreply.github.com>
2024-04-27 17:04:17 -0500

    Usuarios service

--- MicroservicioUsuarios/src/main/java/com/agudelo/usuario/controller/AlumnoController.java
+++ MicroservicioUsuarios/src/main/java/com/agudelo/usuario/controller/AlumnoController.java
@@ -1,9 +1,10 @@
 package com.agudelo.usuario.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,11 +21,24 @@ import com.agudelo.usuario.service.AlumnoService;
 
 
 
+
+
 @RestController
 @RequestMapping("/api/alumno")
 public class AlumnoController {
 	@Autowired
 	AlumnoService service;
+	
+	@Value("${config.balanceador.test}")
+	private String balanceadorTest;
+	
+	@GetMapping("/balanceador-test")
+	public ResponseEntity<?>balanceadorTest(){
+		 Map<String, Object> response = new HashMap<String, Object>();
+		    response.put("balanceador", balanceadorTest);
+		    response.put("alumno", service.findAll());
+		    return ResponseEntity.ok().body(response);
+	}
 	
 	@GetMapping
 	public ResponseEntity<?> listarAlumno(){
